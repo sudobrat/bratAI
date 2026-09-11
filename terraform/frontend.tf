6be9188bdf998c0913a961a18b086945faeaac2b
brat <bharatrajsingal.tech@gmail.com>
2026-09-11 19:40:19 +0530

    Fix: Set Static Web App location to a supported region

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -1,11 +1,9 @@
 resource "azurerm_static_web_app" "frontend" {
   name                = "bratai-frontend"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  
-  # Standard is required if we want to add custom domains later!
-  sku_tier = "Standard" 
-  sku_size = "Standard"
+  location            = "eastasia"
+  sku_tier            = "Standard"
+  sku_size            = "Standard"
 }
 
 # Print out the URL so you know where to visit your app!
