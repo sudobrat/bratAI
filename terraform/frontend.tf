9b4d4d14714b4f138b9de2f47aa22b5e198c83f6
brat <bharatrajsingal.tech@gmail.com>
2026-09-11 21:12:37 +0530

    add: godaddy domain automation

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -11,6 +11,10 @@ output "frontend_url" {
   value = "https://${azurerm_static_web_app.frontend.default_host_name}"
 }
 
+output "frontend_hostname" {
+  value = azurerm_static_web_app.frontend.default_host_name
+}
+
 # Output the API key so GitHub actions can deploy to it!
 output "static_web_app_api_token" {
   value     = azurerm_static_web_app.frontend.api_key
