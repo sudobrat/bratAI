355d2abd3a3d61980e1b8b14122cf6c422126729
brat <bharatrajsingal.tech@gmail.com>
2026-09-11 19:53:03 +0530

    fix: .yml file

--- terraform/env.tf
+++ terraform/env.tf
@@ -11,4 +11,5 @@ resource "azurerm_container_app_environment" "env" {
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.logs.id
+  logs_destination           = "log-analytics"
 }
